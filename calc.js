c3d9af390a49b95444bac8f7ec3285490692a5ab
Luis <luis.hernandez1@ucalgary.ca>
2025-06-18 00:58:25 -0600

    created linked list

--- calc.js
+++ calc.js
@@ -1,7 +1,76 @@
 
+// creating linked list to store all values
+
+// node class
+class node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// Linked list class
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    // Add to the end
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  // Add to the start
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  // Delete first node with matching value
+  delete(value) {
+    if (!this.head) return;
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next && current.next.value !== value) {
+      current = current.next;
+    }
+
+    if (current.next) {
+      current.next = current.next.next;
+    }
+  }
+
+  // Print the list
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.value + " -> ";
+      current = current.next;
+    }
+    result += "null";
+    console.log(result);
+  }
+}
 
 
-
+/* 
 document.getElementById("btn-multiply").onclick = function() {
     console.log("x")
 }
+*/
